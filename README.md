cc347766d20aab67325b21872230a8bc34a9e1c2
Orens Xhagolli <orens.xhagolli@hotmail.com>
2017-11-23 01:45:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Akka Demo
+
+This demo is intended to show usage of Akka Actors in java.
---
+++ src/com/github/oxhagolli/akkademo/Example.java
@@ -0,0 +1,8 @@
+package com.github.oxhagolli.akkademo;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+    }
+}
